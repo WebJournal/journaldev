40b26b19675ce9c2bcfc448be37a840967b97aad
Pankaj 0323 <pankaj.0323@gmail.com>
2014-05-22 21:00:07 +0000

    adding more examples for HQL

--- Hibernate/HQLExample/src/main/java/com/journaldev/hibernate/main/HQLExamples.java
+++ Hibernate/HQLExample/src/main/java/com/journaldev/hibernate/main/HQLExamples.java
@@ -34,6 +34,15 @@ public class HQLExamples {
 		Employee emp = (Employee) query.uniqueResult();
 		System.out.println("Employee Name="+emp.getName()+", City="+emp.getAddress().getCity());
 		
+		//pagination example
+		query = session.createQuery("from Employee");
+		query.setFirstResult(0); //starts with 0
+		query.setFetchSize(2);
+		empList = query.list();
+		for(Employee emp4 : empList){
+			System.out.println("Paginated Employees::"+emp4.getId()+","+emp4.getAddress().getCity());
+		}
+		
 		//Update Employee
 		query = session.createQuery("update Employee set name= :name where id= :id");
 		query.setParameter("name", "Pankaj Kumar");
@@ -63,6 +72,21 @@ public class HQLExamples {
 		List<Object[]> list = query.list();
 		for(Object[] arr : list){
 			System.out.println(Arrays.toString(arr));
+		}
+		
+		//group by and like example
+		query = session.createQuery("select e.name, sum(e.salary), count(e)"
+				+ " from Employee e where e.name like '%i%' group by e.name");
+		List<Object[]> groupList = query.list();
+		for(Object[] arr : groupList){
+			System.out.println(Arrays.toString(arr));
+		}
+		
+		//order by example
+		query = session.createQuery("from Employee e order by e.id desc");
+		empList = query.list();
+		for(Employee emp3 : empList){
+			System.out.println("ID Desc Order Employee::"+emp3.getId()+","+emp3.getAddress().getCity());
 		}
 		
 		//rolling back to save the test data
