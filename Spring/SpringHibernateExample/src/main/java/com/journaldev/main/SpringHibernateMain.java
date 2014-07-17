71aa8290fd2b2bf3c5b8f0aec8868279c9b1e210
Pankaj 0323 <pankaj.0323@gmail.com>
2014-07-17 16:10:13 +0000

    Spring Hibernate Example

---
+++ Spring/SpringHibernateExample/src/main/java/com/journaldev/main/SpringHibernateMain.java
@@ -0,0 +1,35 @@
+package com.journaldev.main;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.journaldev.dao.PersonDAO;
+import com.journaldev.model.Person;
+
+public class SpringHibernateMain {
+
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		PersonDAO personDAO = context.getBean(PersonDAO.class);
+		
+		Person person = new Person();
+		person.setName("Pankaj"); person.setCountry("India");
+		
+		personDAO.save(person);
+		
+		System.out.println("Person::"+person);
+		
+		List<Person> list = personDAO.list();
+		
+		for(Person p : list){
+			System.out.println("Person List::"+p);
+		}
+		
+		context.close();
+		
+	}
+
+}
