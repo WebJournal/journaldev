56c8fd2ed6bcea2183900d858a6ff9d73f8c9802
Pankaj 0323 <pankaj.0323@gmail.com>
2014-08-28 13:18:50 +0000

    Primefaces Spring EclipseLink NoSQL MongoDB Oracle NoSQL example

---
+++ PrimeFaces/Primefaces-EclipseLink-Spring-Mongo-Oracle-NoSQL-Sample/src/main/java/com/journaldev/jpa/data/Employee.java
@@ -0,0 +1,57 @@
+package com.journaldev.jpa.data;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.eclipse.persistence.nosql.annotations.DataFormatType;
+import org.eclipse.persistence.nosql.annotations.Field;
+import org.eclipse.persistence.nosql.annotations.NoSql;
+@Entity
+@NoSql(dataFormat=DataFormatType.MAPPED)
+public class Employee {
+	@Id
+	@GeneratedValue
+	@Field(name="_id")
+	private String employeeId;
+	
+	private String employeeName;
+	
+	private Date employeeHireDate;
+	
+	private double employeeSalary;
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Date getEmployeeHireDate() {
+		return employeeHireDate;
+	}
+
+	public void setEmployeeHireDate(Date employeeHireDate) {
+		this.employeeHireDate = employeeHireDate;
+	}
+
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+}
