767e723f0e0d8448f89a066ce4edbbf4c5c589d7
Pankaj 0323 <pankaj.0323@gmail.com>
2014-09-01 16:51:37 +0000

    Primefaces Select tutorials

---
+++ PrimeFaces/Primefaces-Selection-Samples/src/main/java/com/journaldev/prime/faces/beans/SelectOneMenu.java
@@ -0,0 +1,48 @@
+package com.journaldev.prime.faces.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class SelectOneMenu {
+	private List<String> tutorials = new ArrayList<String>();
+	
+	private String selectedTutorial = "";
+
+	
+	@PostConstruct
+	public void init(){
+		this.tutorials = new ArrayList<String>();
+		this.tutorials.add("Primefaces");
+		this.tutorials.add("Hibernate");
+		this.tutorials.add("Spring");
+	}
+
+	public List<String> getTutorials() {
+		return tutorials;
+	}
+
+	public void setTutorials(List<String> tutorials) {
+		this.tutorials = tutorials;
+	}
+
+	public String getSelectedTutorial() {
+		return selectedTutorial;
+	}
+
+	public void setSelectedTutorial(String selectedTutorial) {
+		this.selectedTutorial = selectedTutorial;
+	}
+
+	public String register(){
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("You've Registered In:", this.selectedTutorial));
+		return "";
+	}
+}
