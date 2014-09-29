d42000f694d022c8e397f373e2bbb55af88daf38
Pankaj 0323 <pankaj.0323@gmail.com>
2014-09-29 15:32:07 +0000

    Portlet and Struts2 integration example

---
+++ Portal-Portlets/EmployeeRegistration-StrutsBridge/src/main/java/com/journaldev/data/Employee.java
@@ -0,0 +1,41 @@
+package com.journaldev.data;
+
+public class Employee {
+	private String id;
+	private String name;
+	private String job;
+	private String salary;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+}
