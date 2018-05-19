33d4765ab1a4d270819e4145f067f076a18ce300
Anupam Chugh <anupam.chugh@gmail.com>
2018-05-19 12:44:17 +0530

    Add files via upload

---
+++ Android/ActionBar/app/src/main/java/com/journaldev/actionbar/MainActivity.java
@@ -0,0 +1,53 @@
+package com.journaldev.actionbar;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView count;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
+        case R.id.add:
+            count=(TextView)findViewById(R.id.textView);
+            count.setText("Add is clicked");
+            return(true);
+        case R.id.reset:
+            count=(TextView)findViewById(R.id.textView);
+            count.setText("Nothing is selected");
+            return(true);
+        case R.id.about:
+            Toast.makeText(this, R.string.about_toast, Toast.LENGTH_LONG).show();
+            return(true);
+        case R.id.exit:
+            finish();
+            return(true);
+
+    }
+        return(super.onOptionsItemSelected(item));
+    }
+
+
+}
---
+++ Android/CustomActionBar/app/src/main/java/com/journaldev/customactionbar/MainActivity.java
@@ -0,0 +1,45 @@
+package com.journaldev.customactionbar;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar_layout);
+
+        View view =getSupportActionBar().getCustomView();
+
+        ImageButton imageButton= (ImageButton)view.findViewById(R.id.action_bar_back);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        ImageButton imageButton2= (ImageButton)view.findViewById(R.id.action_bar_forward);
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Forward Button is clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
