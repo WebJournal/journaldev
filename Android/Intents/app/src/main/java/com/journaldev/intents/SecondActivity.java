ae7e99b56fd30862eafa0ec58c17e46c9119c53d
Anupam Chugh <anupam.chugh@gmail.com>
2018-05-19 12:31:08 +0530

    Added Android-Intents source code.

---
+++ Android/Intents/app/src/main/java/com/journaldev/intents/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.journaldev.intents;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SecondActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+
+}
