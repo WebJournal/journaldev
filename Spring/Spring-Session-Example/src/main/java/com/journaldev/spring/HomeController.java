05051c926da408d6ec9d51f9d13f5808cae3b565
Pankaj Kumar <pankaj.0323@gmail.com>
2018-06-20 17:28:52 +0530

    Spring Session Example - Not Working Right Now

---
+++ Spring/Spring-Session-Example/src/main/java/com/journaldev/spring/HomeController.java
@@ -0,0 +1,40 @@
+package com.journaldev.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(Model model, HttpSession session) {
+		List<String> msgs = (List<String>) session.getAttribute("MY_MESSAGES");
+        if(msgs == null) {
+            msgs = new ArrayList<>();
+        }
+        model.addAttribute("messages", msgs);
+		
+		return "index";
+	}
+	
+	@PostMapping("/messages")
+    public String saveMessage(@RequestParam("msg") String msg, HttpServletRequest request) 
+    {
+        List<String> msgs = (List<String>) request.getSession().getAttribute("MY_MESSAGES");
+        if(msgs == null) {
+            msgs = new ArrayList<>();
+            request.getSession().setAttribute("MY_MESSAGES", msgs);
+        }
+        msgs.add(msg);
+        return "redirect:/";
+    }
+}
---
+++ Spring/Spring-Session-Example/src/main/java/com/journaldev/spring/SpringSessionExampleApplication.java
@@ -0,0 +1,12 @@
+package com.journaldev.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionExampleApplication.class, args);
+	}
+}
