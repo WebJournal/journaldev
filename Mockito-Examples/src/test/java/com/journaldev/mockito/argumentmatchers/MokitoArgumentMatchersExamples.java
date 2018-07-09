b886244cedc3a655c1505033a277381d16ada6b2
Pankaj Kumar <pankaj.0323@gmail.com>
2018-07-09 16:24:29 +0530

    Mockito Argument Matchers examples

---
+++ Mockito-Examples/src/test/java/com/journaldev/mockito/argumentmatchers/MokitoArgumentMatchersExamples.java
@@ -0,0 +1,52 @@
+package com.journaldev.mockito.argumentmatchers;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.AdditionalMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class MokitoArgumentMatchersExamples {
+
+	@Test
+	void test() {
+		Foo mockFoo = mock(Foo.class);
+
+		// Argument Matchers than static arguments
+		when(mockFoo.bool(anyString(), anyInt(), any(Object.class))).thenReturn(true);
+		assertTrue(mockFoo.bool("A", 1, "A"));
+		assertTrue(mockFoo.bool("B", 10, new Object()));
+		assertTrue(mockFoo.bool("false", 10, new Object()));
+
+		// every argument should be argument matcher
+		// use eq() when we have to use static value
+		when(mockFoo.bool(eq("false"), anyInt(), any(Object.class))).thenReturn(false);
+		assertFalse(mockFoo.bool("false", 10, new Object()));
+
+		when(mockFoo.in(anyBoolean(), anyList())).thenReturn(10);
+
+		// Arrays and Additional Matchers for rare cases
+		when(mockFoo.bar(any(byte[].class), aryEq(new String[] { "A", "B" }), gt(10))).thenReturn(11);
+
+		// argument matchers with verify
+		verify(mockFoo, atLeast(0)).bool(anyString(), anyInt(), any(Object.class));
+		verify(mockFoo, atLeast(0)).bool(eq("false"), anyInt(), any(Object.class));
+
+	}
+}
+
+class Foo {
+	boolean bool(String str, int i, Object obj) {
+		return false;
+	}
+
+	int in(boolean b, List<String> strs) {
+		return 0;
+	}
+
+	int bar(byte[] bytes, String[] s, int i) {
+		return 0;
+	}
+}
