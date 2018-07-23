90f62f42bea68b6c5c0ca10aac1ecd41b1405494
Anupam Chugh <anupam.chugh@gmail.com>
2018-07-24 01:03:55 +0530

    Add files via upload

---
+++ Android/AndroidMediaPlayerSong/app/src/main/java/com/journaldev/androidmediaplayersong/MainActivity.java
@@ -0,0 +1,159 @@
+package com.journaldev.androidmediaplayersong;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements Runnable {
+
+
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    SeekBar seekBar;
+    boolean wasPlaying = false;
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        fab = findViewById(R.id.button);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playSong();
+            }
+        });
+
+        final TextView seekBarHint = findViewById(R.id.textView);
+
+        seekBar = findViewById(R.id.seekbar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+                seekBarHint.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+                seekBarHint.setVisibility(View.VISIBLE);
+                int x = (int) Math.ceil(progress / 1000f);
+
+                if (x < 10)
+                    seekBarHint.setText("0:0" + x);
+                else
+                    seekBarHint.setText("0:" + x);
+
+                double percent = progress / (double) seekBar.getMax();
+                int offset = seekBar.getThumbOffset();
+                int seekWidth = seekBar.getWidth();
+                int val = (int) Math.round(percent * (seekWidth - 2 * offset));
+                int labelWidth = seekBarHint.getWidth();
+                seekBarHint.setX(offset + seekBar.getX() + val
+                        - Math.round(percent * offset)
+                        - Math.round(percent * labelWidth / 2));
+
+                if (progress > 0 && mediaPlayer != null && !mediaPlayer.isPlaying()) {
+                    clearMediaPlayer();
+                    fab.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, android.R.drawable.ic_media_play));
+                    MainActivity.this.seekBar.setProgress(0);
+                }
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                    mediaPlayer.seekTo(seekBar.getProgress());
+                }
+            }
+        });
+    }
+
+    public void playSong() {
+
+        try {
+
+
+            if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                clearMediaPlayer();
+                seekBar.setProgress(0);
+                wasPlaying = true;
+                fab.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, android.R.drawable.ic_media_play));
+            }
+
+
+            if (!wasPlaying) {
+
+                if (mediaPlayer == null) {
+                    mediaPlayer = new MediaPlayer();
+                }
+
+                fab.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, android.R.drawable.ic_media_pause));
+
+                AssetFileDescriptor descriptor = getAssets().openFd("suits.mp3");
+                mediaPlayer.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+                descriptor.close();
+
+                mediaPlayer.prepare();
+                mediaPlayer.setVolume(0.5f, 0.5f);
+                mediaPlayer.setLooping(false);
+                seekBar.setMax(mediaPlayer.getDuration());
+
+                mediaPlayer.start();
+                new Thread(this).start();
+
+            }
+
+            wasPlaying = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void run() {
+
+        int currentPosition = mediaPlayer.getCurrentPosition();
+        int total = mediaPlayer.getDuration();
+
+
+        while (mediaPlayer != null && mediaPlayer.isPlaying() && currentPosition < total) {
+            try {
+                Thread.sleep(1000);
+                currentPosition = mediaPlayer.getCurrentPosition();
+            } catch (InterruptedException e) {
+                return;
+            } catch (Exception e) {
+                return;
+            }
+
+            seekBar.setProgress(currentPosition);
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        clearMediaPlayer();
+    }
+
+    private void clearMediaPlayer() {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+}
