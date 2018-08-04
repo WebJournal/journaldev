ca1518bd5b1c3777517d8523befc5e70b43a6dcd
Anupam Chugh <anupam.chugh@gmail.com>
2018-08-04 20:32:13 +0530

    Updated MVVM source code

---
+++ Android/AndroidMVVMBasics/app/src/main/java/com/journaldev/androidmvvmbasics/viewmodels/LoginViewModel.java
@@ -0,0 +1,48 @@
+package com.journaldev.androidmvvmbasics.viewmodels;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import com.android.databinding.library.baseAdapters.BR;
+import com.journaldev.androidmvvmbasics.model.User;
+
+public class LoginViewModel extends BaseObservable {
+    private User user;
+
+
+    private String successMessage = "Login was successful";
+    private String errorMessage = "Email or Password not valid";
+
+    @Bindable
+    public String toastMessage = null;
+
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+
+    private void setToastMessage(String toastMessage) {
+
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    public LoginViewModel() {
+        user = new User("", "");
+    }
+
+    public void afterEmailTextChanged(CharSequence s) {
+        user.setEmail(s.toString());
+    }
+
+    public void afterPasswordTextChanged(CharSequence s) {
+        user.setPassword(s.toString());
+    }
+
+    public void onLoginClicked() {
+        if (user.isInputDataValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+}
