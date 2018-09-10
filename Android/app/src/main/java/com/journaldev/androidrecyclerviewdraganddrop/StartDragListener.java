13335d0caa3b07bc025154e5dd1ee780016a6f17
Anupam Chugh <anupam.chugh@gmail.com>
2018-09-10 13:06:05 +0530

    AndroidRecyclerViewDragDrop removed

--- Android/app/src/main/java/com/journaldev/androidrecyclerviewdraganddrop/StartDragListener.java
+++
@@ -1,7 +0,0 @@
-package com.journaldev.androidrecyclerviewdraganddrop;
-
-import android.support.v7.widget.RecyclerView;
-
-public interface StartDragListener {
-    void requestDrag(RecyclerView.ViewHolder viewHolder);
-}
