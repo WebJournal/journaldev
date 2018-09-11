3b489788f5abba6a3d806d450b5e499956945d1d
Anupam Chugh <anupam.chugh@gmail.com>
2018-09-11 23:23:27 +0530

    Huffman code added

--- Data-Structure-Algorithms/src/main/java/com/journaldev/huffmancoding/HuffmanCodeSolution.java
+++ Data-Structure-Algorithms/src/main/java/com/journaldev/huffmancoding/HuffmanCodeSolution.java
@@ -1,4 +1,144 @@
 package com.journaldev.huffmancoding;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class HuffmanCodeSolution {
+
+
+    private static HuffmanNode buildTree(int[] charFrequencies) {
+
+        PriorityQueue<HuffmanNode> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < charFrequencies.length; i++) {
+
+
+            HuffmanNode huffmanNode = new HuffmanNode();
+            huffmanNode.data = (char) i;
+            huffmanNode.frequency = charFrequencies[i];
+            huffmanNode.left = null;
+            huffmanNode.right = null;
+            priorityQueue.offer(huffmanNode);
+        }
+        assert priorityQueue.size() > 0;
+
+        while (priorityQueue.size() > 1) {
+
+
+            HuffmanNode x = priorityQueue.peek();
+            priorityQueue.poll();
+
+            HuffmanNode y = priorityQueue.peek();
+            priorityQueue.poll();
+
+            HuffmanNode sum = new HuffmanNode();
+
+            sum.frequency = x.frequency + y.frequency;
+            sum.data = '-';
+
+            sum.left = x;
+
+            sum.right = y;
+            root = sum;
+
+            priorityQueue.offer(sum);
+        }
+
+        return priorityQueue.poll();
+    }
+
+    private static Map<Character, String> charPrefixHashMap = new HashMap<>();
+    static HuffmanNode root;
+
+    private static void setPrefixCodes(HuffmanNode tree, StringBuilder prefix) {
+
+
+        if (tree.left == null && tree.right == null && Character.isLetter(tree.data)) {
+            charPrefixHashMap.put(tree.data, prefix.toString());
+
+        } else {
+            prefix.append('0');
+            setPrefixCodes(tree.left, prefix);
+            prefix.deleteCharAt(prefix.length() - 1);
+
+
+            prefix.append('1');
+            setPrefixCodes(tree.right, prefix);
+            prefix.deleteCharAt(prefix.length() - 1);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+        String test = "ABACA";
+        String s = encoding(test);
+        decode(s);
+
+    }
+
+    private static String encoding(String test) {
+        int[] charFrequencies = new int[256];
+        for (char c : test.toCharArray())
+            charFrequencies[c]++;
+
+        root = buildTree(charFrequencies);
+
+        setPrefixCodes(root, new StringBuilder());
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < test.length(); i++) {
+            char c = test.charAt(i);
+            s.append(charPrefixHashMap.get(c));
+        }
+
+        return s.toString();
+    }
+
+    private static void decode(String s) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        HuffmanNode temp = root;
+
+        System.out.println("Encoded: " + s);
+
+        for (int i = 0; i < s.length(); i++) {
+            int j = Integer.parseInt(String.valueOf(s.charAt(i)));
+
+            if (j == 0) {
+                temp = temp.left;
+                if (temp.left == null && temp.right == null) {
+                    stringBuilder.append(temp.data);
+                    temp = root;
+                }
+            }
+            if (j == 1) {
+                temp = temp.right;
+                if (temp.left == null && temp.right == null) {
+                    stringBuilder.append(temp.data);
+                    temp = root;
+                }
+            }
+        }
+
+        System.out.println("Decoded string is " + stringBuilder.toString());
+
+
+    }
 }
+
+class HuffmanNode implements Comparable<HuffmanNode> {
+    int frequency;
+    char data;
+    HuffmanNode left, right;
+
+    public int compareTo(HuffmanNode tree) {
+        return frequency - tree.frequency;
+    }
+}
+
+
+
