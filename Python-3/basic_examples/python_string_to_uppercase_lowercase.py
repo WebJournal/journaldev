cb6bfcbcd38df67f5767ffa60bd7f634f5175383
Pankaj Kumar <pankaj.0323@gmail.com>
2018-09-27 13:03:24 +0530

    Python string to uppercase, lowercase examples

---
+++ Python-3/basic_examples/python_string_to_uppercase_lowercase.py
@@ -0,0 +1,13 @@
+s = '987abcDEF%$'
+
+# print('Uppercase String =', s.upper())
+print('Lowercase String =', s.lower())
+print('Original String =', s)
+
+s = input('Please Provide Input String\n')
+
+print('Input String in Lowercase =', s.lower())
+
+# print('Input String in Uppercase =', s.upper())
+
+print('Original String =', s)
