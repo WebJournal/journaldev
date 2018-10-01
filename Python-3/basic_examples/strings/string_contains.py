4db585cf03f4d423ee2d6c5d548a63f035203d05
Pankaj Kumar <pankaj.0323@gmail.com>
2018-10-01 12:07:36 +0530

    Python string contains examples

---
+++ Python-3/basic_examples/strings/string_contains.py
@@ -0,0 +1,11 @@
+s = 'abc'
+
+print('s contains a =', s.__contains__('a'))
+print('s contains A =', s.__contains__('A'))
+print('s contains X =', s.__contains__('X'))
+
+input_str1 = input('Please enter first input string\n')
+
+input_str2 = input('Please enter second input string\n')
+
+print('First Input String Contains Second String? ', input_str1.__contains__(input_str2))
