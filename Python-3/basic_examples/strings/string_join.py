2c8ec0e5a8d1145b05776f2b3c0d1c90c5bf84fe
Pankaj Kumar <pankaj.0323@gmail.com>
2018-10-08 13:29:47 +0530

    Python string join() examples

---
+++ Python-3/basic_examples/strings/string_join.py
@@ -0,0 +1,16 @@
+s = 'abc'
+
+s1 = s.join('xyz')
+
+print(s1)
+
+s1 = ','.join(['a', 'b', 'c'])
+
+print(s1)
+
+# concatenate strings using join on empty string
+s1 = ''.join(['a', 'b', 'c'])
+print(s1)
+
+s1 = ''.join(('a', 'b', 'c'))
+print(s1)
