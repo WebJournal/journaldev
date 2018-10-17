2615b4388f8386da01d1a64dca12cd7885151167
Pankaj Kumar <pankaj.0323@gmail.com>
2018-10-17 11:30:08 +0530

    Python remove character from string

---
+++ Python-3/basic_examples/strings/string_remove_characters.py
@@ -0,0 +1,24 @@
+s = 'abc12321cba'
+
+print(s.replace('a', ''))
+
+s = 'abc12321cba'
+
+print(s.translate({ord('a'): None}))
+
+print(s.translate({ord(i): None for i in 'abc'}))
+
+# removing spaces from a string
+s = ' 1 2 3 4 '
+print(s.replace(' ', ''))
+print(s.translate({ord(i): None for i in ' '}))
+
+# remove substring from string
+s = 'ab12abc34ba'
+print(s.replace('ab', ''))
+
+# remove newline
+s = 'ab\ncd\nef'
+print(s.replace('\n', ''))
+print(s.translate({ord('\n'): None}))
+
