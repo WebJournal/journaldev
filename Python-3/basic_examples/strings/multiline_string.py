e99c78f8347c778534ff1f514e2e1423e09ef858
Pankaj Kumar <pankaj.0323@gmail.com>
2018-11-08 11:09:07 +0530

    python multiline string

---
+++ Python-3/basic_examples/strings/multiline_string.py
@@ -0,0 +1,25 @@
+s = 'My Name is Pankaj.\nI am the owner of JournalDev.com\nJournalDev is a very popular website in Developers community.'
+
+print(s)
+
+s = """My Name is Pankaj.
+I am the owner of JournalDev.com
+JournalDev is a very popular website in Developers community."""
+
+print(s)
+
+s = ("My Name is Pankaj. "
+     "I am the owner of JournalDev.com and "
+     "JournalDev is a very popular website in Developers community.")
+print(s)
+
+s = "My Name is Pankaj. " \
+    "I am the owner of JournalDev.com and " \
+    "JournalDev is a very popular website in Developers community."
+print(s)
+
+s = ' '.join(("My Name is Pankaj. I am the owner of",
+              "JournalDev.com and",
+              "JournalDev is a very popular website",
+              "in Developers community."))
+print(s)
