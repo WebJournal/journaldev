b1cacd3584d5d3aa131b366c0346c1ba248b73e9
Pankaj Kumar <pankaj.0323@gmail.com>
2018-11-20 10:34:13 +0530

    python string capitalize() function

---
+++ Python-3/basic_examples/strings/string_capitalize.py
@@ -0,0 +1,13 @@
+s = 'WelCome to JournalDev'
+
+print(s.capitalize())
+
+s = '歡迎'  # welcome in Chinese
+print(s.capitalize())
+
+s = 'çA†'
+print(s.capitalize())
+
+s = input('Please enter a string:\n')
+
+print('Input String Capitalized Version:\n', s.capitalize())
