f6e1a2a1518b391024e1fec03184b656019eab0e
Pankaj Kumar <pankaj.0323@gmail.com>
2018-11-22 16:03:03 +0530

    python string center() function

---
+++ Python-3/basic_examples/strings/string_center.py
@@ -0,0 +1,17 @@
+s = 'Hello'
+
+s1 = s.center(20)
+print(f'***{s1}***')
+
+s1 = s.center(20, '#')
+print(f'***{s1}***')
+
+s1 = s.center(20, 'ç')
+print(f'***{s1}***')
+
+# s.center('#')  # TypeError: 'str' object cannot be interpreted as an integer
+
+# s1 = s.center()  # TypeError: center() takes at least 1 argument (0 given)
+
+# s.center(20, '#$')  # TypeError: The fill character must be exactly one character long
+
