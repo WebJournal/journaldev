635a6ca90e54bf1328daf17783466c10ecbc1e35
Pankaj Kumar <pankaj.0323@gmail.com>
2018-11-22 16:10:15 +0530

    python string center() function updates

--- Python-3/basic_examples/strings/string_center.py
+++ Python-3/basic_examples/strings/string_center.py
@@ -9,6 +9,9 @@ print(f'***{s1}***')
 s1 = s.center(20, 'ç')
 print(f'***{s1}***')
 
+s1 = s.center(4)
+print(f'***{s1}***')  # when string size is more than specified width, original string is returned
+
 # s.center('#')  # TypeError: 'str' object cannot be interpreted as an integer
 
 # s1 = s.center()  # TypeError: center() takes at least 1 argument (0 given)
