de1af21e241356814db5247842ad443dd899da60
Anupam Chugh <anupam.chugh@agricxlab.com>
2018-11-29 13:42:08 +0530

    AndroidRecyclerView DataBinding first half

---
+++ Android/AndroidRecyclerViewDataBinding/app/src/main/java/com/journaldev/androidrecyclerviewdatabinding/MainActivity.java
@@ -0,0 +1,40 @@
+package com.journaldev.androidrecyclerviewdatabinding;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.journaldev.androidrecyclerviewdatabinding.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        binding.recyclerView.setLayoutManager(linearLayoutManager);
+
+        populateData();
+    }
+
+    private void populateData() {
+        List<DataModel> dataModelList = new ArrayList<>();
+
+        dataModelList.add(new DataModel("Android Oreo", "8.1"));
+        dataModelList.add(new DataModel("Android Pie", "9.0"));
+        dataModelList.add(new DataModel("Android Nougat", "7.0"));
+        dataModelList.add(new DataModel("Android Marshmallow", "6.0"));
+
+        binding.recyclerView.setAdapter(new MyRecyclerViewAdapter(dataModelList, this));
+    }
+}
