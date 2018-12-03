e3ec77af0f0501424bf44cb7b116f8ad9f53314e
Pankaj Kumar <pankaj.0323@gmail.com>
2018-12-03 14:49:37 +0530

    python string isalnum() function examples

---
+++ Python-3/basic_examples/strings/string_isalnum.py
@@ -0,0 +1,15 @@
+s = 'Hello World 2019'
+
+print(s.isalnum())
+
+s = 'HelloWorld2019'
+print(s.isalnum())
+
+s = ''
+print(s.isalnum())
+
+s='A.B'
+print(s.isalnum())
+
+s = '10.50'
+print(s.isalnum())
