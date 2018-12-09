c46c2fd4de125bbbf90543ba1253634f7ce236a0
Anupam Chugh <anupam.chugh@agricxlab.com>
2018-12-09 12:34:36 +0530

    Android RecyclerView Load More Endless scrollingtutorial

---
+++ Android/AndroidRecyclerViewLoadMore/app/src/main/java/com/journaldev/androidrecyclerviewloadmore/MainActivity.java
@@ -0,0 +1,100 @@
+package com.journaldev.androidrecyclerviewloadmore;
+
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+    ArrayList<String> rowsArrayList = new ArrayList<>();
+
+    boolean isLoading = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        populateData();
+        initAdapter();
+        initScrollListener();
+
+
+    }
+
+    private void populateData() {
+        int i = 0;
+        while (i < 10) {
+            rowsArrayList.add("Item " + i);
+            i++;
+        }
+    }
+
+    private void initAdapter() {
+
+        recyclerViewAdapter = new RecyclerViewAdapter(rowsArrayList);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    private void initScrollListener() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == rowsArrayList.size() - 1) {
+                        //bottom of list!
+                        loadMore();
+                        isLoading = true;
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    private void loadMore() {
+        rowsArrayList.add(null);
+        recyclerViewAdapter.notifyItemInserted(rowsArrayList.size() - 1);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                rowsArrayList.remove(rowsArrayList.size() - 1);
+                int scrollPosition = rowsArrayList.size();
+                recyclerViewAdapter.notifyItemRemoved(scrollPosition);
+                int currentSize = scrollPosition;
+                int nextLimit = currentSize + 10;
+
+                while (currentSize - 1 < nextLimit) {
+                    rowsArrayList.add("Item " + currentSize);
+                    currentSize++;
+                }
+
+                recyclerViewAdapter.notifyDataSetChanged();
+                isLoading = false;
+            }
+        }, 2000);
+
+
+    }
+}
---
+++ Android/AndroidRecyclerViewLoadMore/app/src/main/java/com/journaldev/androidrecyclerviewloadmore/RecyclerViewAdapter.java
@@ -0,0 +1,101 @@
+package com.journaldev.androidrecyclerviewloadmore;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final int VIEW_TYPE_ITEM = 0;
+    private final int VIEW_TYPE_LOADING = 1;
+
+    public List<String> mItemList;
+
+
+    public RecyclerViewAdapter(List<String> itemList) {
+
+        mItemList = itemList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_ITEM) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+            return new ItemViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
+            return new LoadingViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+
+        if (viewHolder instanceof ItemViewHolder) {
+
+            populateItemRows((ItemViewHolder) viewHolder, position);
+        } else if (viewHolder instanceof LoadingViewHolder) {
+            showLoadingView((LoadingViewHolder) viewHolder, position);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItemList == null ? 0 : mItemList.size();
+    }
+
+    /**
+     * The following method decides the type of ViewHolder to display in the RecyclerView
+     *
+     * @param position
+     * @return
+     */
+    @Override
+    public int getItemViewType(int position) {
+        return mItemList.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
+    }
+
+
+    private class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvItem;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvItem = itemView.findViewById(R.id.tvItem);
+        }
+    }
+
+    private class LoadingViewHolder extends RecyclerView.ViewHolder {
+
+        ProgressBar progressBar;
+
+        public LoadingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progressBar);
+        }
+    }
+
+    private void showLoadingView(LoadingViewHolder viewHolder, int position) {
+        //ProgressBar would be displayed
+
+    }
+
+    private void populateItemRows(ItemViewHolder viewHolder, int position) {
+
+        String item = mItemList.get(position);
+        viewHolder.tvItem.setText(item);
+
+    }
+
+
+}
