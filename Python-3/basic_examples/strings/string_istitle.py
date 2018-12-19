47e30e2fa0bbb8a53a33e02bbec3c08dccd5e9b4
Pankaj Kumar <pankaj.0323@gmail.com>
2018-12-19 13:44:38 +0530

    python string istitle() function examples

---
+++ Python-3/basic_examples/strings/string_istitle.py
@@ -0,0 +1,34 @@
+s = 'A Big Cat'
+print(s.istitle())
+
+s = 'A BIG Cat'
+print(s.istitle())
+
+s = 'A big Cat'
+print(s.istitle())
+
+s = 'a big cat'
+print(s.istitle())
+
+s = 'A'
+print(s.istitle())
+
+s = ''
+print(s.istitle())
+
+# special characters
+s = 'Â Ɓig Ȼat'
+print(s.istitle())
+
+
+import unicodedata
+
+count = 0
+for codepoint in range(2 ** 16):
+    ch = chr(codepoint)
+    if ch.istitle():
+        print(u'{:04x}: {} ({})'.format(codepoint, ch, unicodedata.name(ch, 'UNNAMED')))
+        count = count + 1
+print(f'Total Number of Space Unicode Characters = {count}')
+
+
