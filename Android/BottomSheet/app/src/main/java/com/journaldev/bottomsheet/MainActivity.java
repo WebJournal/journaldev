ac532e10614d5abe0f3e520f25c680d26c32c642
Anupam Chugh <anupam.chugh@agricxlab.com>
2018-12-23 22:59:14 +0530

    Android BottomSheet Added

---
+++ Android/BottomSheet/app/src/main/java/com/journaldev/bottomsheet/MainActivity.java
@@ -0,0 +1,88 @@
+package com.journaldev.bottomsheet;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements ItemAdapter.ItemListener {
+
+
+    BottomSheetBehavior behavior;
+    RecyclerView recyclerView;
+    private ItemAdapter mAdapter;
+    CoordinatorLayout coordinatorLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+
+
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        behavior = BottomSheetBehavior.from(bottomSheet);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                // React to state change
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                // React to dragging events
+            }
+        });
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        ArrayList<String> items = new ArrayList<>();
+        items.add("Item 1");
+        items.add("Item 2");
+        items.add("Item 3");
+        items.add("Item 4");
+        items.add("Item 5");
+        items.add("Item 6");
+
+        mAdapter = new ItemAdapter(items, this);
+        recyclerView.setAdapter(mAdapter);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+    }
+
+    @Override
+    public void onItemClick(String item) {
+
+        Snackbar.make(coordinatorLayout,item + " is selected", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+
+        behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+    }
+}
