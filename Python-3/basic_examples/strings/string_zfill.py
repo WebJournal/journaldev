501940812b7f7d781bcc9d2a923b689b37e1e2fc
Pankaj Kumar <pankaj.0323@gmail.com>
2019-01-03 23:42:03 +0530

    python string zfill() function examples

---
+++ Python-3/basic_examples/strings/string_zfill.py
@@ -0,0 +1,17 @@
+s = '100'
+print(s.zfill(6))
+
+s = '+100'
+print(s.zfill(6))
+
+s = '-100'
+print(s.zfill(6))
+
+s = '+100'
+print(s.zfill(3))
+
+s = 'abc'
+print(s.zfill(6))
+
+s = '+abc'
+print(s.zfill(6))
