3fcea1074686fb03d56dd575fc91152d64bd4140
Pankaj Kumar <pankaj.0323@gmail.com>
2019-01-03 11:55:17 +0530

    python string rsplit() function examples

--- Python-3/basic_examples/strings/string_split.py
+++ Python-3/basic_examples/strings/string_split.py
@@ -34,3 +34,33 @@ input_csv_split_list = input_csv.split(sep=',')
 print('Input Data Length =', len(input_csv_split_list))
 print('List of inputs =', input_csv_split_list)
 
+# rsplit() example
+s = 'Python is Awesome'
+str_list = s.rsplit(sep=' ')
+print(str_list)
+
+str_list = s.rsplit(sep=' ', maxsplit=1)
+print(str_list)
+
+s = '  Java  Python iOS    Android  '
+str_list = s.rsplit()
+print(str_list)
+
+multiline_str = 'Hi There\nHow are you?\nI am fine'
+multiline_str_split_list = multiline_str.rsplit(sep='\n')
+for s in multiline_str_split_list:
+    print(s)
+
+s = 'Hi||Hello||Adios'
+str_list = s.rsplit('||')
+print(str_list)
+
+# using split() with str class
+print(str.rsplit('ABACAD', sep='A'))
+print(str.rsplit('ABACAD', sep='A', maxsplit=2))
+
+# csv and user input example
+input_csv = input('Please enter CSV Data\n')
+input_csv_split_list = input_csv.rsplit(sep=',')
+print('Input Data Length =', len(input_csv_split_list))
+print('List of inputs =', input_csv_split_list)
