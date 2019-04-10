5cbc3c8f4e2f21078b606df019e4c5e3950a775a
Anupam Chugh <anupam.chugh@agricxlab.com>
2019-04-11 00:46:30 +0530

    AndroidPNotifications updated AndroidX

--- Android/AndroidPNotifications/app/src/main/java/com/journaldev/androidpnotifications/MainActivity.java
+++ Android/AndroidPNotifications/app/src/main/java/com/journaldev/androidpnotifications/MainActivity.java
@@ -3,7 +3,6 @@ package com.journaldev.androidpnotifications;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.RemoteInput;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
