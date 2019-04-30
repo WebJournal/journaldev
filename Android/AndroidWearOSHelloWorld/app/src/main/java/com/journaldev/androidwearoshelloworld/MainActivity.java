1cdfaca0711ee64f6ad17761ab62be78df343a25
Anupam Chugh <anupam.chugh@agricxlab.com>
2019-05-01 01:49:29 +0530

    Android Wear OS Tutorial

---
+++ Android/AndroidWearOSHelloWorld/app/src/main/java/com/journaldev/androidwearoshelloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.journaldev.androidwearoshelloworld;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.text);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
